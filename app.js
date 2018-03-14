5ed868001612b1d7dfd86a4574d6cbe80f769899
Charles McQuain <charlesmcquain@gmail.com>
2018-03-13 22:46:04 -0700

    Added Session and Authentication

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// Load User Model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
 
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+// Map Global Promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose.connect(keys.mongoURI)
+.then(() =>  console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It Works!');
+});
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
 app.use('/auth', auth);
